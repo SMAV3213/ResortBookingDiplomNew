204ddb9f8cf5fa0738b1911f51e1e0d03dbddc40
User <User@KOMPUTER>
2026-01-15 20:33:10 +0800

    Исправление Di

---
+++ ResortBooking.API/DependencyInjection.cs
@@ -0,0 +1,172 @@
+﻿using FluentValidation;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using ResortBooking.API.Filters;
+using ResortBooking.Application.Interfaces.Repositories;
+using ResortBooking.Application.Interfaces.Services;
+using ResortBooking.Infrastructure.Persistence;
+using ResortBooking.Infrastructure.Repositories;
+using ResortBooking.Infrastructure.Services;
+using System.Reflection;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace ResortBooking.API;
+
+/// <summary>
+/// Инъекция зависимостей.
+/// </summary>
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services
+            .AddEndpointsApiExplorer()
+            .AddControllers()
+            .AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(
+                    new JsonStringEnumConverter()
+                );
+            });
+
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection"));
+        });
+
+
+        services.AddMemoryCache();
+        services.AddApiAuthorization(configuration);
+        services.AddApiSwagger();
+
+        //Репозитории
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
+        services.AddScoped<IRoomTypeRepository, RoomTypeRepository>();
+        services.AddScoped<IRoomRepository, RoomRepository>();
+        services.AddScoped<IBookingRepository, BookingRepository>();
+        //Сервисы
+        services.AddScoped<IAuthService, AuthService>();
+        services.AddScoped<IRoomTypeService, RoomTypeService>();
+        services.AddScoped<IRoomService, RoomService>();
+        services.AddScoped<IBookingService, BookingService>();
+        services.AddScoped<IUserService, UserService>();
+
+        services.AddValidatorsFromAssembly(
+            Assembly.GetExecutingAssembly(),
+            ServiceLifetime.Scoped
+        );
+
+        return services;
+    }
+
+    public static IServiceCollection AddApiAuthorization(
+    this IServiceCollection services, IConfiguration configuration
+    )
+    {
+        using var provider = services.BuildServiceProvider();
+        var jwtSettings = configuration.GetSection("Jwt");
+
+        services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+
+                ValidIssuer = jwtSettings["Issuer"],
+                ValidAudience = jwtSettings["Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(jwtSettings["Key"]!))
+            };
+        });
+
+        return services;
+    }
+
+    private static IServiceCollection AddApiSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            var currentAssembly = Assembly.GetExecutingAssembly();
+
+            var xmlDocs = currentAssembly.GetReferencedAssemblies()
+                .Union([currentAssembly.GetName()])
+                .Select(a => Path.Combine(AppContext.BaseDirectory, $"{a.Name}.xml"))
+                .Where(f => File.Exists(f)).ToList();
+
+            xmlDocs.ForEach(xmlDoc => options.IncludeXmlComments(xmlDoc));
+
+            options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
+            {
+                Description = "Введите JWT токен авторизации.",
+                Name = "Authorization",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                BearerFormat = "JWT",
+                Scheme = JwtBearerDefaults.AuthenticationScheme
+            });
+
+            //options.DocumentFilter<InfoFilter>();
+            options.OperationFilter<JwtAuthorizeFilter>();
+        });
+
+        return services;
+    }
+
+    public static WebApplication UseApiServices(this WebApplication app,
+    IWebHostEnvironment webHostEnvironment)
+    {
+        app.UseExceptionHandler(options => { });
+
+        app.MapControllers();
+
+        app.UseHttpsRedirection();
+        app.UseAuth();
+        app.UseStaticFiles();
+        app.UseApiSwagger(webHostEnvironment);
+
+        return app;
+    }
+
+    private static WebApplication UseAuth(this WebApplication app)
+    {
+        //app.UseCors("cors-policy");
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        return app;
+    }
+
+    private static WebApplication UseApiSwagger(this WebApplication app,
+    IWebHostEnvironment webHostEnvironment)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty;
+            options.EnableDeepLinking();
+            options.DocumentTitle =
+                $"({(webHostEnvironment.IsProduction() ? "Prod" : "Dev")}) ResortBooking REST API";
+        });
+
+        return app;
+    }
+}
--- ResortBooking.API/Program.cs
+++ ResortBooking.API/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi;
+using ResortBooking.API;
 using ResortBooking.API.Filters;
 using ResortBooking.Application.Interfaces.Repositories;
 using ResortBooking.Application.Interfaces.Services;
@@ -14,92 +15,14 @@ using System.Reflection;
 using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
-var jwtSettings = builder.Configuration.GetSection("Jwt");
-builder.Services.AddEndpointsApiExplorer(); 
-builder.Services.AddSwaggerGen(options =>
-{
-    var currentAssembly = Assembly.GetExecutingAssembly();
 
-    var xmlDocs = currentAssembly.GetReferencedAssemblies()
-        .Union([currentAssembly.GetName()])
-        .Select(a => Path.Combine(AppContext.BaseDirectory, $"{a.Name}.xml"))
-        .Where(f => File.Exists(f)).ToList();
-
-    xmlDocs.ForEach(xmlDoc => options.IncludeXmlComments(xmlDoc));
-
-    options.AddSecurityDefinition(JwtBearerDefaults.AuthenticationScheme, new OpenApiSecurityScheme
-    {
-        Description = "Введите JWT токен авторизации.",
-        Name = "Authorization",
-        In = ParameterLocation.Header,
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = JwtBearerDefaults.AuthenticationScheme
-    });
-
-    //options.DocumentFilter<InfoFilter>();
-    options.OperationFilter<JwtAuthorizeFilter>();
-});
-// Add services to the container.
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-
-        ValidIssuer = jwtSettings["Issuer"],
-        ValidAudience = jwtSettings["Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(
-            Encoding.UTF8.GetBytes(jwtSettings["Key"]!))
-    };
-});
-builder.Services.AddAuthorization();
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-//builder.Services.AddOpenApi();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-//Репозитории
-builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
-builder.Services.AddScoped<IRoomTypeRepository, RoomTypeRepository>();
-builder.Services.AddScoped<IRoomRepository, RoomRepository>();
-builder.Services.AddScoped<IBookingRepository, BookingRepository>();
-//Сервисы
-builder.Services.AddScoped<IAuthService, AuthService>();
-builder.Services.AddScoped<IRoomTypeService, RoomTypeService>();
-builder.Services.AddScoped<IRoomService, RoomService>();
-builder.Services.AddScoped<IBookingService, BookingService>();
-builder.Services.AddScoped<IUserService, UserService>();
+builder.Services
+    .AddApiServices(builder.Configuration);
+    //.AddApplicationServices()
+    //.AddInfrastructureServices(builder.Configuration, builder.Environment)
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger(); 
-    app.UseSwaggerUI(); 
-}
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseApiServices(app.Environment);
 
 app.Run();
